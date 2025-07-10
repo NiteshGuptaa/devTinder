f26984dbc7965ad16d99c334c592cf1e284cd9d3
NiteshGuptaa <nkgupta62093@gmail.com>
2025-07-10 12:17:54 +0530

    exploring GET POST DELETE methods

--- homework.md
+++ homework.md
@@ -1,2 +1,10 @@
- - create git repository
- - create server
+ - Create git repository
+ - Initialize the repository
+ - Install Express
+ - Create a Server
+ - Listen to port (3000)
+ - Write requist handler
+ - Install nodemon ans update scripts inside package.json
+
+ - Create routes
+ - Order of routes matter a lot
--- src/app.js
+++ src/app.js
@@ -2,17 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) =>{
-    res.send("Namaste from the Dashbord!");
-});
+// app.use("/", (req, res) =>{  //! it will override the paths
+//     res.send("Namaste from the Dashbord!");
+// });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello route!");
-});
+//! order of routes is very important
 
+// this is match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server!");
 });
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) =>{
+    res.send({firstName: "Nitesh", lastName: "Kumar"})
+})
+
+app.post("/user", (req, res) =>{
+    // saving data to DB
+    res.send("Data successfully saved to database!")
+})
+
+
+
+
 
 app.listen(4000, ()=>{
     console.log("Server is successfully listening on 4000 port...");
