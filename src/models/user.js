2914d8acd5ac61c166c401d2bf424fb915013666
NiteshGuptaa <nkgupta62093@gmail.com>
2025-07-18 13:32:05 +0530

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,51 @@ const mongoose = require("mongoose");
 
 // const {Schema} = mongoose;
 
-const userSchema = new mongoose.Schema({
-  firstName: {type: String, required: true, minLength: 4, maxLength: 50},
-  lastName: { type: String },
-  emailId: { type: String, required: true, unique: true, trim: true},
-  password: { type: String, required: true},
-  age: { type: Number, min: 18, max: 60},
-  gender: { 
-    type: String, 
-    validate(value){
-      if(!["male", "female", "other"].includes(value)){
-        throw new Error("Gender data is not valide!!")
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { 
+      type: String, 
+      required: true, 
+      minLength: 4, 
+      maxLength: 50 
+    },
+    lastName: { 
+      type: String 
+    },
+    emailId: { 
+      type: String, 
+      required: true, 
+      unique: true, 
+      trim: true 
+    },
+    password: { 
+      type: String, 
+      required: true 
+    },
+    age: { 
+      type: Number, 
+      min: 18, 
+      max: 60 
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valide!!");
+        }
+      },
+    },
+    about: { 
+      type: String, 
+      default: "This is the defualt about the user!!" 
+    },
+    skills: {
+      type:[String],
     }
   },
-  about: {type: String, default: "Engineer"}
-},
-{
-  timestamps: true
-}
+  {
+    timestamps: true,
+  }
 );
 
 // create models
